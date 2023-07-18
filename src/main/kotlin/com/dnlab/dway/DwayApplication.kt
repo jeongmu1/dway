b8d63598c63e5eb4a0c5e1051eced58ed8c13ed8
Kang <Kang815595@gmail.com>
2023-07-18 10:16:55 +0900

    chore: 프로젝트 생성

---
+++ src/main/kotlin/com/dnlab/dway/DwayApplication.kt
@@ -0,0 +1,11 @@
+package com.dnlab.dway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DwayApplication
+
+fun main(args: Array<String>) {
+    runApplication<DwayApplication>(*args)
+}
