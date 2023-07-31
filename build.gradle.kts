2e2b1d6caa10733f7c4bf734fe8a669acb221f91
kang815595@gmail.com <Kang815595@gmail.com>
2023-07-31 22:01:16 +0900

    fix: JWT 라이브러리 의존성 scope 변경
    - 테스트 시 사용을위해 implementation scope 로 설정해두었으나 테스트에 사용하지 안게 되어 compileOnly, runtimeOnly로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,9 +46,9 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
 
     // JWT
-    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
-    implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    compileOnly("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
     // API
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
