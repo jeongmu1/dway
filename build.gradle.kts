fbb1480a5d792e50a8351b7779ef60409809c6c7
kang815595@gmail.com <Kang815595@gmail.com>
2023-10-05 21:19:58 +0900

    feat: Kotest 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ configurations {
 
 group = "com.dnlab"
 version = "0.0.1-SNAPSHOT"
+val kotestVersion = "5.7.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -78,6 +79,13 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
+
+    // Kotlin Test
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testImplementation("io.mockk:mockk:1.12.0")
 
     // Documentation
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
