d11bc33177fd328ca9e1cdbd64963cee50b83907
kang815595@gmail.com <Kang815595@gmail.com>
2023-10-05 21:21:16 +0900

    refactor: JUnit 기반 테스트를 Kotest 기반으로 변경
    - 토큰 재발급 테스트코드를 Kotest 기반으로 변경

--- src/test/kotlin/com/dnlab/dway/auth/service/RefreshTokenTest.kt
+++ src/test/kotlin/com/dnlab/dway/auth/service/RefreshTokenTest.kt
@@ -1,74 +1,74 @@
 package com.dnlab.dway.auth.service
 
 import com.dnlab.dway.auth.config.JwtProperties
+import com.dnlab.dway.auth.domain.Authority
+import com.dnlab.dway.auth.domain.Role
 import com.dnlab.dway.auth.domain.Token
 import com.dnlab.dway.auth.dto.request.RefreshTokenRequestDto
 import com.dnlab.dway.auth.exception.InvalidTokenException
 import com.dnlab.dway.auth.repository.AuthorityRepository
-import com.dnlab.dway.auth.repository.MemberRepository
 import com.dnlab.dway.auth.repository.TokenRepository
-import com.dnlab.dway.region.repository.CountryRepository
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.mockito.Mock
-import org.mockito.Mockito.*
-import org.mockito.MockitoAnnotations.openMocks
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.crypto.password.PasswordEncoder
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import org.mockito.kotlin.any
 
-internal class RefreshTokenTest {
-    private lateinit var authService: AuthService
+internal class RefreshTokenTest : FunSpec({
+    val tokenRepository = mockk<TokenRepository>()
+    val authorityRepository = mockk<AuthorityRepository>()
 
-    private val tokenService: TokenService =
-        TokenService(
-            JwtProperties(
-                secret = "asdfhakjlsfhdjklfhadfkjadlsfjkdasfhajklsadhfjkasdlkjhasdjlfhaslds",
-                header = "header",
-                accessTokenExpiration = 1000L,
-                refreshTokenExpiration = 1000L
-            )
+    val tokenService = TokenService(
+        JwtProperties(
+            secret = "asdfhakjlsfhdjklfhadfkjadlsfjkdasfhajklsadhfjkasdlkjhasdjlfhaslds",
+            header = "header",
+            accessTokenExpiration = 1000L,
+            refreshTokenExpiration = 1000L
         )
+    )
 
-    @Mock
-    private lateinit var tokenRepository: TokenRepository
+    val authService = AuthServiceImpl(
+        tokenService = tokenService,
+        memberRepository = mockk(),
+        authorityRepository = authorityRepository,
+        passwordEncoder = mockk(),
+        tokenRepository = tokenRepository,
+        countryRepository = mockk(),
+        authenticationManagerBuilder = mockk()
+    )
 
-    @BeforeEach
-    fun setUp() {
-        openMocks(this)
-        authService = AuthServiceImpl(
-            tokenService = tokenService,
-            memberRepository = mock(MemberRepository::class.java),
-            authorityRepository = mock(AuthorityRepository::class.java),
-            passwordEncoder = mock(PasswordEncoder::class.java),
-            tokenRepository = tokenRepository,
-            countryRepository = mock(CountryRepository::class.java),
-            authenticationManagerBuilder = mock(AuthenticationManagerBuilder::class.java)
-        )
-    }
+    context("토큰 재발급") {
+        test("올바른 토큰이 입력되었을 경우 널이 아닌 값을 반환해야 한다") {
+            val refreshToken = "refreshToken"
 
-    @Test
-    fun refreshToken_success() {
-        val refreshToken = "refreshToken"
-
-        `when`(tokenRepository.findTokenByToken(anyString())).thenReturn(
-            Token(
+            every { tokenRepository.findTokenByToken(any()) } returns Token(
                 token = refreshToken,
                 username = "username"
             )
-        )
 
-        assertNotNull(authService.refreshToken(RefreshTokenRequestDto(refreshToken)))
+            every { tokenRepository.delete(any()) } returns any()
+            every { tokenRepository.save(any()) } returns any()
+            every { authorityRepository.findAuthoritiesByMemberUsername(any()) } returns setOf(
+                Authority(
+                    id = 0L,
+                    member = mockk(),
+                    role = Role.ROLE_MEMBER
+                )
+            )
+
+            authService.refreshToken(RefreshTokenRequestDto(refreshToken)) shouldNotBe null
+        }
+
+        test("올바르지 않은 토큰이 입력되었을 경우 예외를 던져야 한다") {
+            val refreshToken = "wrongToken"
+
+            every { tokenRepository.findTokenByToken(any()) } returns null
+            every { tokenRepository.delete(any()) } returns any()
+
+            shouldThrow<InvalidTokenException> {
+                authService.refreshToken(RefreshTokenRequestDto(refreshToken))
+            }
+        }
     }
-
-    @Test
-    fun refreshToken_invalidToken() {
-        val refreshToken = "wrongToken"
-
-        `when`(tokenRepository.findTokenByToken(anyString())).thenReturn(null)
-
-        assertThrows<InvalidTokenException> { authService.refreshToken(RefreshTokenRequestDto(refreshToken)) }
-    }
-
-}
+})
